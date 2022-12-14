0441740ff12ad18383bab43668fd8d73808c8c42
Lovisa Reiche <lovisa.reiche@gmx.de>
2022-12-14 14:16:52 +0000

    code for chart firstwave.jpg

---
+++ 1_code/data_firstwave.R
@@ -0,0 +1,159 @@
+# ------------
+# Introduction
+# ------------
+
+## Create a chart to show when survey participants joined the panel
+
+rm(list=ls())
+NAME <- 'data_firstwave' ## Name of the R file goes here (without the file extension!)
+PROJECT <- 'EmpiricalGenderGap'
+PROJECT_DIR <- 'D:/Lovisa/Studium/Oxford/Department of Economics/DPhil' ## Change this to the directory in which your project folder is located, make sure to avoid using single backslashes (i.e. \)!
+
+# ------------
+# Preamble
+# ------------
+
+## -------
+## Imports
+## -------
+### All the library imports go here
+
+library('tidyverse')
+library("viridis")  # colour
+
+## --------
+## Settings
+## --------
+### Any settings go here
+
+
+## ---------------------
+## Set working directory
+## ---------------------
+### The code below will traverse the path upwards until it finds the root folder of the project.
+
+setwd(file.path(PROJECT_DIR, PROJECT))
+
+## ----------------------------------
+## Set  up pipeline folder if missing
+## ----------------------------------
+### The code below will automatically create a pipeline folder for this code file if it does not exist.
+
+if (dir.exists(file.path('empirical', '2_pipeline'))){
+  pipeline <- file.path('empirical', '2_pipeline', NAME)
+} else {
+  pipeline <- file.path('2_pipeline', NAME)
+}
+
+if (!dir.exists(pipeline)) {
+  dir.create(pipeline)
+  for (folder in c('out', 'store', 'tmp')){
+    dir.create(file.path(pipeline, folder))
+  }
+}
+
+
+### The code below will automatically create an output folder for this code file if it does not exist.
+
+if (!dir.exists(file.path('empirical', '3_output','results',NAME))) {
+  dir.create(file.path('empirical', '3_output','results',NAME))
+}
+
+
+# ---------
+# Functions
+# ---------
+
+
+# ---------
+# Main code
+# ---------
+
+
+## -- Load data from pipeline folder --
+
+T <- read_csv(file.path('empirical', '2_pipeline', 'code03_compilepanel.m','out','base', 'T.csv')) 
+
+
+
+## -- Create firstwave vector
+
+
+W <- mutate(T,firstwave = floor(T$id/100000)) %>%
+  group_by(wave,firstwave) %>%
+  summarise(obsfirstwave = n()) %>%
+  ungroup 
+ 
+
+## -- Fill missing values
+
+wave <- sort(unique(W$wave))
+firstwave <- sort(unique(W$firstwave))
+
+W_full <- matrix(0, nrow = length(firstwave),ncol = length(wave))
+for (i in 1:length(wave)){
+  w <- wave[i]
+  for (ii in 1:length(firstwave)) {
+    o <- firstwave[ii]
+    if (any(W$wave == w & W$firstwave == o)) {
+      W_full[ii,i] <- W$obsfirstwave[W$wave == w & W$firstwave == o]
+    }
+  }
+}
+
+wnames <- paste("w",wave,sep = "")
+fwnames <- paste("w",firstwave,sep = "")
+
+## -- Save Bar Plot
+
+jpeg(file.path('empirical','3_output','results', NAME,"firstwave.jpg"), width = 1000, height = 700)
+
+
+barplot(W_full, main = "Wave of first survey participation", names.arg = wnames,
+        xlab = "Observations", ylab = "Wave", col = viridis(length(firstwave)))
+legend("topleft", fwnames, fill = viridis(length(firstwave)))
+
+dev.off()
+
+
+# --- Comparing models
+
+
+# settings for stargazer
+notes <- "The full set of estimators included can be found in the appendix."
+omit <- c("w\\d","y>=","si\\_")
+omit.labels <- c("Time dummies","Constants","Shop intent")
+title <- "Eplaining financial literacy through time variant and invariant variables"
+label <- "tab:meanexplainlit"
+dep.var.labels <- c("Subjective financial literacy","Financial literacy test score")
+
+# which variables are removed
+rmvars <- c(str_subset(names(coef(lsubj)),"w\\d"),str_subset(names(coef(lsubj)),"y>="),str_subset(names(coef(lsubj)),"si\\_"))
+# which are staying
+stayvars <- setdiff(names(coef(lsubj)),rmvars)
+# in which order
+desiredOrder <- c("female","live_alone","live_alone_fem",
+                  "shop_groceries","shop_major","prep_meals",
+                  "decide_finance","pessimist","prob_intqr","refresher",
+                  "nround","f_nointerest", "f_easy","eduschool","eduwork",
+                  "hhchildren","hhinc","pinc","age","citysize",
+                  "eastgerman","east1989","part_time","unemployed","retired")
+
+writeLines(capture.output(stargazer(lsubj, lsubjtv, ltest, ltesttv,
+                                    title = title, notes = notes, label = label, 
+                                    omit = omit, omit.labels = omit.labels, 
+                                    model.names = FALSE, 
+                                    align=TRUE , df = FALSE, digits = 2, header = FALSE, 
+                                    order = desiredOrder, intercept.top = TRUE, intercept.bottom = FALSE, 
+                                    dep.var.labels = dep.var.labels, no.space = TRUE)), 
+           file.path('empirical','3_output','results', NAME,t,'code_meanexplainlit.tex'))
+
+
+
+# ----------
+# Leftovers
+# ----------
+## Here you leave any code snippets or temporary code that you don't need but don't want to delete just yet
+
+
+ 
