5669122a7826784694575d97703546396285ae63
Lovisa Reiche <lovisa.reiche@gmx.de>
2022-12-19 14:17:22 +0000

    save in 4 different folders

--- 1_code/data_histogramming.R
+++ 1_code/data_histogramming.R
@@ -26,7 +26,8 @@ library("viridis")  # colour
 ## --------
 ### Any settings go here
 
-s <- "female"
+s <- "hhcluster"
+# run "female", "pessimist", "subj_finilliterate","hhcluster"
 
 ## ---------------------
 ## Set working directory
@@ -75,6 +76,8 @@ if (!dir.exists(file.path('empirical', '3_output','results',NAME,s))) {
 load(file.path('empirical', '2_pipeline', 'code08_fitlit','out', 'T.RData'))
 hhcluster <- read_csv(file.path('empirical', '2_pipeline', 'cluster.m','out', 'hhcluster.csv'))
 T["hhcluster"] <- hhcluster
+T["pessimist"] <- as.numeric(T$pessimist>=3)
+T["subj_finilliterate"] <- as.numeric(T$pred_subj_bin==0)
 
 ## -- Separate inflation expectations by s
 
@@ -83,9 +86,13 @@ y2 <- T$y[T[s]==1]
 
 ## --- Draw histogram
 
-jpeg(file.path('empirical','3_output','results', NAME,paste("histogram_",s,".jpg")), width = 1000, height = 700)
+jpeg(file.path('empirical','3_output','results', NAME,s,paste("histogram_",s,".jpg", sep = "")), width = 1000, height = 700)
 
-hist(y1, breaks = c(min(y1),-10,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,20,30,max(y1)), col = alpha('#238a8DFF',0.8), main = paste("Histogram of Inflation Expectations:" , s, sep = " "))
-hist(y2, breaks = c(min(y2),-10,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,20,30,max(y2)), col = alpha('#FDE725FF',0.7), add = TRUE)
+hist(y1, breaks = c(min(y1),-10,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,20,30,max(y1)), 
+     col = alpha('#238a8DFF',0.8), main = paste("Histogram of Inflation Expectations:" , s, sep = " "),
+     xlim = c(-10,30))
+hist(y2, breaks = c(min(y2),-10,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,20,30,max(y2)), 
+     col = alpha('#FDE725FF',0.7), 
+     xlim = c(-10,30), add = TRUE)
 legend("topleft", c(paste("not",s, sep = " "),s), fill = c('#238a8DFF','#FDE725FF'))
 dev.off()
