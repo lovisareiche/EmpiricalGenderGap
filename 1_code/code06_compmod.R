cacb038d6a382e1fb11bf6cf6499c561e4d4b03e
Lovisa Reiche <lovisa.reiche@gmx.de>
2023-01-03 13:52:47 +0000

    comment out unused lines

--- 1_code/code06_compmod.R
+++ 1_code/code06_compmod.R
@@ -34,7 +34,7 @@ library(miceadds) # for loading individual files
 ### Any settings go here
 
 t <- 'LSDVt_control'
-l <- 'level'
+l <- 'log'
 
 ## ---------------------
 ## Set working directory
@@ -108,14 +108,14 @@ extractorRData <- function(file, object) {
 
 base <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','base',l, 'T.RData'),paste('y.',t, sep = ""))
 int <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','int',l, 'T.RData'),paste('y.',t, sep = ""))
-edu <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','no_edu',l, 'T.RData'),paste('y.',t, sep = ""))
-employ <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','no_employ',l, 'T.RData'),paste('y.',t, sep = ""))
-geo <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','no_geo',l, 'T.RData'),paste('y.',t, sep = ""))
+#edu <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','no_edu',l, 'T.RData'),paste('y.',t, sep = ""))
+#employ <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','no_employ',l, 'T.RData'),paste('y.',t, sep = ""))
+#geo <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','no_geo',l, 'T.RData'),paste('y.',t, sep = ""))
 hhroles <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','no_hhroles',l, 'T.RData'),paste('y.',t, sep = ""))
-quali <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','no_quali',l, 'T.RData'),paste('y.',t, sep = ""))
+#quali <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','no_quali',l, 'T.RData'),paste('y.',t, sep = ""))
 pessimist <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','no_pessimist',l, 'T.RData'),paste('y.',t, sep = ""))
-inc <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','no_inc',l, 'T.RData'),paste('y.',t, sep = ""))
-shopintent <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','no_shopintent',l, 'T.RData'),paste('y.',t, sep = ""))
+#inc <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','no_inc',l, 'T.RData'),paste('y.',t, sep = ""))
+#shopintent <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','no_shopintent',l, 'T.RData'),paste('y.',t, sep = ""))
 uncertainty <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','no_uncertainty',l, 'T.RData'),paste('y.',t, sep = ""))
 nocontrol  <- extractorRData(file.path('empirical', '2_pipeline', 'code04_regress','out','demo_only',l, 'T.RData'),paste('y.',t, sep = ""))
 
@@ -158,14 +158,14 @@ writeLines(capture.output(stargazer(base, int, pessimist, uncertainty, hhroles, 
            file.path('empirical','3_output','results', NAME,t,l, 'code_compmod.tex'))
 
 
-label <- "tab:compmoddemo"
-writeLines(capture.output(stargazer(quali, shopintent, geo, edu, inc,
-                                    title = title, notes = notes, label = label, 
-                                    omit = omit, omit.labels = omit.labels, 
-                                    model.names = FALSE, 
-                                    align=TRUE , df = FALSE, digits = 2, header = FALSE, 
-                                    order = desiredOrder, intercept.top = TRUE, intercept.bottom = FALSE, 
-                                    dep.var.labels = dep.var.labels, no.space = TRUE)), 
-           file.path('empirical','3_output','results', NAME,t,l, 'code_compmod_demo.tex'))
-
+# label <- "tab:compmoddemo"
+# writeLines(capture.output(stargazer(quali, shopintent, geo, edu, inc,
+#                                     title = title, notes = notes, label = label, 
+#                                     omit = omit, omit.labels = omit.labels, 
+#                                     model.names = FALSE, 
+#                                     align=TRUE , df = FALSE, digits = 2, header = FALSE, 
+#                                     order = desiredOrder, intercept.top = TRUE, intercept.bottom = FALSE, 
+#                                     dep.var.labels = dep.var.labels, no.space = TRUE)), 
+#            file.path('empirical','3_output','results', NAME,t,l, 'code_compmod_demo.tex'))
+# 
 
