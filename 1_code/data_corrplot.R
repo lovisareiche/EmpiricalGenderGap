9660ee1601e0e138239034efcc7ef87d2135499d
Lovisa Reiche <lovisa.reiche@gmx.de>
2023-02-02 11:15:18 +0000

    correlation table instead of plot

--- 1_code/data_corrplot.R
+++ 1_code/data_corrplot.R
@@ -22,6 +22,8 @@ library('tidyverse')
 library("viridis")  # colour
 library(corrplot)
 library(PerformanceAnalytics)
+library(Hmisc)
+library(xtable)
 
 ## --------
 ## Settings
@@ -94,6 +96,55 @@ panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...) {
        cex = 1 + cex.cor * Cor) # Resize the text by level of correlation
 }
 
+# Function to create correlation table with significance stars
+# x is a matrix containing the data
+# method : correlation method. "pearson"" or "spearman"" is supported
+# removeTriangle : remove upper or lower triangle
+# results :  if "html" or "latex"
+# the results will be displayed in html or latex format
+corstars <-function(x, method=c("pearson", "spearman"), removeTriangle=c("upper", "lower"),
+                    result=c("none", "html", "latex")){
+  #Compute correlation matrix
+  require(Hmisc)
+  x <- as.matrix(x)
+  correlation_matrix<-rcorr(x, type=method[1])
+  R <- correlation_matrix$r # Matrix of correlation coeficients
+  p <- correlation_matrix$P # Matrix of p-value 
+  
+  ## Define notions for significance levels; spacing is important.
+  mystars <- ifelse(p < .0001, "****", ifelse(p < .001, "*** ", ifelse(p < .01, "**  ", ifelse(p < .05, "*   ", "    "))))
+  
+  ## trunctuate the correlation matrix to two decimal
+  R <- format(round(cbind(rep(-1.11, ncol(x)), R), 2))[,-1]
+  
+  ## build a new matrix that includes the correlations with their apropriate stars
+  Rnew <- matrix(paste(R, mystars, sep=""), ncol=ncol(x))
+  diag(Rnew) <- paste(diag(R), " ", sep="")
+  rownames(Rnew) <- colnames(x)
+  colnames(Rnew) <- paste(colnames(x), "", sep="")
+  
+  ## remove upper triangle of correlation matrix
+  if(removeTriangle[1]=="upper"){
+    Rnew <- as.matrix(Rnew)
+    Rnew[upper.tri(Rnew, diag = TRUE)] <- ""
+    Rnew <- as.data.frame(Rnew)
+  }
+  
+  ## remove lower triangle of correlation matrix
+  else if(removeTriangle[1]=="lower"){
+    Rnew <- as.matrix(Rnew)
+    Rnew[lower.tri(Rnew, diag = TRUE)] <- ""
+    Rnew <- as.data.frame(Rnew)
+  }
+  
+  ## remove last column and return the correlation matrix
+  Rnew <- cbind(Rnew[1:length(Rnew)-1])
+  if (result[1]=="none") return(Rnew)
+  else{
+    if(result[1]=="html") print(xtable(Rnew), type="html")
+    else print(xtable(Rnew), type="latex") 
+  }
+} 
 
 # Change the place_points function within the corrplot function. To do so, run:
 #   trace(corrplot, edit=TRUE)
@@ -117,10 +168,6 @@ panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...) {
 # Main code
 # ---------
 
-#change the corrplot function as described above 
-trace(corrplot, edit=TRUE)
-
-
 ## -- Load data from pipeline folder --
 # select variables for correlation plot
 
@@ -128,6 +175,14 @@ T <- read_csv(file.path('empirical', '2_pipeline', 'code03_compilepanel.m','out'
 female <- T$female
 T <-  select(T, 'non_single','shop_groceries_nsing','shop_major_nsing', 'prep_meals_nsing', 'decide_finance_nsing','pessimist','prob_intqr','f_nointerest','f_easy','refresher','nround')
 res1 <- cor.mtest(T, conf.level = .95)
+
+
+# Make correlation table
+
+corstars(T, result="latex")
+
+#change the corrplot function as described above 
+trace(corrplot, edit=TRUE)
 
 ## -- Save Bar Plot
 
