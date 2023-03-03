f141f0a92afd43fe6f83a5015715d8358da9dd38
Lovisa Reiche <lovisa.reiche@gmx.de>
2023-03-03 11:24:50 +0000

    redo quant regression as regression over sverela deciles

--- 1_code/compsur_code07_quantreg.R
+++ 1_code/compsur_code07_quantreg.R
@@ -69,7 +69,7 @@ if (!dir.exists(pipeline)) {
 if (dir.exists(file.path('empirical', '3_output','results',f,NAME))){
   outline <- file.path('empirical', '3_output','results',f,NAME)
 } else {
-  outline <- file.path('3_output','results',f,NAME)
+  outline <- file.path('empirical','3_output','results',f,NAME)
 }
 
 if (!dir.exists(file.path('empirical', '3_output','results',f,NAME))) {
@@ -88,9 +88,30 @@ if (!dir.exists(file.path('empirical', '3_output','results',f,NAME))) {
 
 ## -- Load data from pipeline folder --
 
+C_female <- data.frame()
+SE_female <- data.frame()
+C_single <- data.frame()
+SE_single <- data.frame()
+C_fs <- data.frame()
+SE_fs <- data.frame()
+C_age <- data.frame()
+SE_age <- data.frame()
+C_eduschool <- data.frame()
+SE_eduschool <- data.frame()
+C_hhinc <- data.frame()
+SE_hhinc <- data.frame()
+O <- data.frame()
+Q <- data.frame()
+R <- data.frame()
+AR <- data.frame()
+
 for(i in 1:length(S)){
   T <- read_csv(file.path('empirical', '2_pipeline',f, 'code01_align','out',S[i], 'T.csv')) %>%
-    mutate(survey = S[i], date = as.yearmon(paste(year, month), format = "%Y %m"))
+    mutate(survey = S[i], date = as.yearmon(paste(year, month), format = "%Y %m")) %>%
+    pdata.frame( index=c( "id", "date" ) )
+  
+  T <- T[!duplicated(T[c('id','date')]), ]
+  
   assign(paste("T_",S[i],sep = ""),T)
   if(i==1){
     F <- T
@@ -105,7 +126,7 @@ for(i in 1:length(S)){
     setdiff('month') %>%
     setdiff('y') %>%
     setdiff(c('survey','id','date'))
-  xtinames <- c("female","region")
+  xtinames <- c("female","region","eduschool")
   xtvnames <- setdiff(xnames,xtinames)
   
   # bind between effects
@@ -114,7 +135,6 @@ for(i in 1:length(S)){
     xtvnames,
     "id",
     center = "mean",
-    suffix_demean = "_within",
     suffix_groupmean = "_between",
     add_attributes = TRUE,
     verbose = TRUE
@@ -124,36 +144,115 @@ for(i in 1:length(S)){
     pdata.frame(index=c( "id", "date"))
   
   # formula
-  form <- as.formula(paste('y ~','factor(year) +', paste(xnames, collapse='+')))
+  form <- as.formula(paste('y ~','factor(date) + female:single + ', paste(xnames, collapse='+'), '+', paste(paste(xtvnames,"_between",sep = ""), collapse='+')))
   
+  if(S[i]=="Michigan"){
+    form <- as.formula(paste('y ~','factor(date) + female:single + ', paste(xnames, collapse='+')))
+    }
+  
+  for (ii in c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)) {
+    m <- plm( form, data=filter(T_c, y<=quantile(y,ii)), effect = "individual", model = "pooling")
+    assign(paste("m_",ii,sep = ""),m)
+    
+    # collect all coefficient estimates and standard errors for variables of interest
+    C_female[i,ii*10] <- coefficients(m)["female"]
+    SE_female[i,ii*10] <- coefficients(summary(m))[, "Std. Error"]["female"]
+    C_single[i,ii*10] <- coefficients(m)["single"]
+    SE_single[i,ii*10] <- coefficients(summary(m))[, "Std. Error"]["single"]
+    C_fs[i,ii*10] <- coefficients(m)["female:single"]
+    SE_fs[i,ii*10] <- coefficients(summary(m))[, "Std. Error"]["female:single"]
+    C_age[i,ii*10] <- coefficients(m)["age"]
+    SE_age[i,ii*10] <- coefficients(summary(m))[, "Std. Error"]["age"]
+    C_eduschool[i,ii*10] <- coefficients(m)["eduschool"]
+    SE_eduschool[i,ii*10] <- coefficients(summary(m))[, "Std. Error"]["eduschool"]
+    C_hhinc[i,ii*10] <- coefficients(m)["hhinc"]
+    SE_hhinc[i,ii*10] <- coefficients(summary(m))[, "Std. Error"]["hhinc"]
+    
+    # collect observations, percentiles, R2 and adjuster R2
+    O[i,ii*10] <- nrow(filter(T_c, y<=quantile(y,ii)))
+    Q[i,ii*10] <- quantile(T_c$y,ii)
+    h <- summary(m)["r.squared"]
+    R[i,ii*10] <- h$r.squared["rsq"]
+    AR[i,ii*10] <- h$r.squared["adjrsq"]
+  }
+  
+
   # run quantile regression for range of taus
-  qreg <- rq(form, data = T_c, tau = c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9))
-  assign(paste("qreg_",S[i],sep = ""),qreg)
+  # qreg <- rq(form, data = T_c, tau = c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9))
+  # assign(paste("qreg_",S[i],sep = ""),qreg)
   
   # run baseline to compute R2
-  qreg0 <- rq(y ~ 1, data = T_c, tau = c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9))
-  rho <- function(u,tau=c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9))u*(tau - (u < 0))
-  R2 <- 1 - qreg$rho/qreg0$rho
-  assign(paste("R2_",S[i],sep = ""),R2)
+  #qreg0 <- rq(y ~ 1, data = T_c, tau = c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9))
+  #rho <- function(u,tau=c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9))u*(tau - (u < 0))
+  #R2 <- 1 - qreg$rho/qreg0$rho
+  #assign(paste("R2_",S[i],sep = ""),R2)
   
-  # s.qreg <- summary.rqs(qreg)
-  # assign(paste("sqreg_",S[i],sep = ""),s.qreg)
+  #s.qreg <- summary.rq(qreg)
+  #assign(paste("sqreg_",S[i],sep = ""),s.qreg)
   # 
   # jpeg(file.path(outline,paste("qreg_female_",S[i],".jpg",sep = "")), width = 1000, height = 700)
   # plot.summary.rqs(s.qreg, parm = "female", ols = TRUE)
   # dev.off()
   
-  save(qreg, R2,  file = paste(pipeline,"/out/qreg_",S[i],".R.Data",sep = ""))
+  #save(qreg, R2, s.qreg, file = paste(pipeline,"/out/qreg_",S[i],".R.Data",sep = ""))
+  
+  # write latex
+  
+  # settings for stargazer
+  omit <- c("factor","between")
+  omit.labels <- c("Time dummies","Between effects")
+  title <- "Qunatile regression"
+  label <- paste("tab:quantreg",S[i],sep="")
+  dep.var.labels <- "Inflation expectation, 12 months ahead, point estimate"
+  column.labels <- c("Bottom 20%", "Bottom 40%", "Bottom 60%", "Bottom 80%", "Full Sample")
+  
+  # in which order
+  desiredOrder <- c("Constant","female","single","age","eduschool","hhinc")
+  
+  writeLines(capture.output(stargazer(m_0.2,m_0.4,m_0.6,m_0.8,m_1,
+                                      title = title, label = label, 
+                                      omit = omit, omit.labels = omit.labels, 
+                                      model.names = FALSE, column.labels = column.labels,
+                                      align=TRUE , df = FALSE, digits = 2, header = FALSE, 
+                                      order = desiredOrder, intercept.top = TRUE, intercept.bottom = FALSE, 
+                                      dep.var.labels = dep.var.labels)), 
+             file.path(outline, paste("code_quantreg",S[i],".txt",sep="")))
   
 }
 
 rm(T)
 
-## Load BOP HH
+## Format to use in pgf plot
 
-load(paste(pipeline,"/out/qreg_","BOP-HH",".R.Data",sep = ""))
+BOP <- data.frame(t(C_female[1,]),t(C_single[1,]),t(C_age[1,]),t(C_eduschool[1,]),t(C_hhinc[1,]),t(SE_female[1,]),t(SE_single[1,]), t(SE_age[1,]),t(SE_eduschool[1,]),t(SE_hhinc[1,]),t(O[1,])) %>%
+  dplyr::rename(female = X1, single = X1.1, age = X1.2, eduschool = X1.3, hhinc = X1.4,female_se = X1.5, single_se = X1.6, age_se = X1.7, eduschool_se = X1.8, hhinc_se = X1.9, obs = X1.10) %>%
+  mutate(female_u = female + female_se, single_u = single + single_se, age_u = age + age_se, eduschool_u = eduschool + eduschool_se, hhinc_u = hhinc + hhinc_se) %>%
+  mutate(female_l = female - female_se, single_l = single - single_se, age_l = age - age_se, eduschool_l = eduschool - eduschool_se, hhinc_l = hhinc - hhinc_se) %>%
+  mutate(decile = c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)) %>%
+  mutate(decileval = c(quantile(`T_BOP-HH`$y,0.1),quantile(`T_BOP-HH`$y,0.2),quantile(`T_BOP-HH`$y,0.3),quantile(`T_BOP-HH`$y,0.4),quantile(`T_BOP-HH`$y,0.5),quantile(`T_BOP-HH`$y,0.6),quantile(`T_BOP-HH`$y,0.7),quantile(`T_BOP-HH`$y,0.8),quantile(`T_BOP-HH`$y,0.9),quantile(`T_BOP-HH`$y,1)))
 
-fem <- qreg[["coefficients"]]["female",]
-F <- coefficients(qreg)
-summary.rqs(qreg, se="boot")
 
+MSC <- data.frame(t(C_female[2,]),t(C_single[2,]),t(C_age[2,]),t(C_eduschool[2,]),t(C_hhinc[2,]),t(SE_female[2,]),t(SE_single[2,]), t(SE_age[2,]),t(SE_eduschool[2,]),t(SE_hhinc[2,]),t(O[2,])) %>%
+  dplyr::rename(female = X2, single = X2.1, age = X2.2, eduschool = X2.3, hhinc = X2.4,female_se = X2.5, single_se = X2.6, age_se = X2.7, eduschool_se = X2.8, hhinc_se = X2.9, obs = X2.10) %>%
+  mutate(female_u = female + female_se, single_u = single + single_se, age_u = age + age_se, eduschool_u = eduschool + eduschool_se, hhinc_u = hhinc + hhinc_se) %>%
+  mutate(female_l = female - female_se, single_l = single - single_se, age_l = age - age_se, eduschool_l = eduschool - eduschool_se, hhinc_l = hhinc - hhinc_se) %>%
+  mutate(decile = c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)) %>%
+  mutate(decileval = c(quantile(T_Michigan$y,0.1),quantile(T_Michigan$y,0.2),quantile(T_Michigan$y,0.3),quantile(T_Michigan$y,0.4),quantile(T_Michigan$y,0.5),quantile(T_Michigan$y,0.6),quantile(T_Michigan$y,0.7),quantile(T_Michigan$y,0.8),quantile(T_Michigan$y,0.9),quantile(T_Michigan$y,1)))
+
+
+SCE <- data.frame(t(C_female[3,]),t(C_single[3,]),t(C_age[3,]),t(C_eduschool[3,]),t(C_hhinc[3,]),t(SE_female[3,]),t(SE_single[3,]), t(SE_age[3,]),t(SE_eduschool[3,]),t(SE_hhinc[3,]),t(O[3,])) %>%
+  dplyr::rename(female = X3, single = X3.1, age = X3.2, eduschool = X3.3, hhinc = X3.4,female_se = X3.5, single_se = X3.6, age_se = X3.7, eduschool_se = X3.8, hhinc_se = X3.9, obs = X3.10) %>%
+  mutate(female_u = female + female_se, single_u = single + single_se, age_u = age + age_se, eduschool_u = eduschool + eduschool_se, hhinc_u = hhinc + hhinc_se) %>%
+  mutate(female_l = female - female_se, single_l = single - single_se, age_l = age - age_se, eduschool_l = eduschool - eduschool_se, hhinc_l = hhinc - hhinc_se) %>%
+  mutate(decile = c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)) %>%
+  mutate(decileval = c(quantile(T_FRBNY$y,0.1),quantile(T_FRBNY$y,0.2),quantile(T_FRBNY$y,0.3),quantile(T_FRBNY$y,0.4),quantile(T_FRBNY$y,0.5),quantile(T_FRBNY$y,0.6),quantile(T_FRBNY$y,0.7),quantile(T_FRBNY$y,0.8),quantile(T_FRBNY$y,0.9),quantile(T_FRBNY$y,1)))
+
+
+
+# save as delimited text file
+
+library(caroline) # for delimited text file
+
+write.delim(BOP, file = file.path(pipeline, 'out', 'BOP.txt'), sep = "\t")
+write.delim(MSC, file = file.path(pipeline, 'out', 'MSC.txt'), sep = "\t")
+write.delim(SCE, file = file.path(pipeline, 'out', 'SCE.txt'), sep = "\t")
